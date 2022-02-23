a1bf450bda6476d13efa0408f056871784260f5e
ChenYn <highestopchen@gmail.com>
2022-02-23 11:18:13 +0800

    chore: update

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,15 @@
 import './styles/global.css';
 import React from 'react';
 import * as ReactDOM from 'react-dom';
-import { HashRouter, Route, Switch } from 'react-router-dom';
+import { HashRouter, Route } from 'react-router-dom';
 
 import App from './app/App';
-import { ConfigProvider } from 'antd';
-import zhCN from 'antd/lib/locale/zh_CN';
 
 ReactDOM.render(
   <React.StrictMode>
-    <ConfigProvider locale={zhCN}>
-      <HashRouter>
-        <Switch>
-          <Route component={App} />
-        </Switch>
-      </HashRouter>
-    </ConfigProvider>
+    <HashRouter>
+      <Route element={App} />
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
