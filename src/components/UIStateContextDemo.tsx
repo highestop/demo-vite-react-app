974d0043ba21122e84cb9f1772629d6f68c5647d
ChenYn <highestopchen@gmail.com>
2022-08-08 11:20:53 +0800

    mod: UIStateContextDemo

--- src/components/UIStateContextDemo.tsx
+++ src/components/UIStateContextDemo.tsx
@@ -5,81 +5,77 @@ export const App = () => {
   return <Outer></Outer>
 }
 
+// 字段
 export enum UIState {
-  SomeState = 'someState',
-  AnotherState = 'anotherState'
+    PopupState = 'popupState',
+    AnotherState = 'anotherState'
 }
 
+// 类型
 export interface UIStateTypeMap {
-  [UIState.SomeState]: string
-  [UIState.AnotherState]: number
+    [UIState.PopupState]: string
+    [UIState.AnotherState]: number
 }
 
+// 默认值
 export const UIStateInitialValue: { [key in UIState]: UIStateTypeMap[key] } = {
-  [UIState.SomeState]: 'unset',
-  [UIState.AnotherState]: 0
+    [UIState.PopupState]: 'unset',
+    [UIState.AnotherState]: 0
 }
 
-const UIStateKeys = Object.values(UIState)
+export const UIStateKeys = Object.values(UIState)
 
 interface UIStateContext {
-  getState: <T extends UIState>(stateKey: T) => UIStateTypeMap[T]
-  setState: <T extends UIState>(
-    stateKey: T
-  ) => Dispatch<SetStateAction<UIStateTypeMap[T]>>
+    getState: <T extends UIState>(stateKey: T) => UIStateTypeMap[T]
+    setState: <T extends UIState>(stateKey: T) => Dispatch<SetStateAction<UIStateTypeMap[T]>>
 }
 
+// 用于创建 Context 的 InnerHook
 function useUIStateContext(): UIStateContext {
-  const stateMap: Map<UIState, UIStateTypeMap[UIState]> = new Map()
-  const setStateMap: Map<
-    UIState,
-    Dispatch<SetStateAction<UIStateTypeMap[UIState]>> | any
-  > = new Map()
+    // 两个 Map 分别存储所有 stateKey 的 state 属性和 setState 方法
+    const stateMap: Map<UIState, UIStateTypeMap[UIState]> = new Map()
+    const setStateMap: Map<UIState, Dispatch<SetStateAction<UIStateTypeMap[UIState]>> | any> = new Map()
 
-  UIStateKeys.forEach(<T extends UIState>(stateKey: T) => {
-    const [state, setState] = useState<UIStateTypeMap[T]>(
-      UIStateInitialValue[stateKey]
-    )
-    stateMap.set(stateKey, state)
-    setStateMap.set(stateKey, setState)
-  })
+    // 遍历所有 stateKeys，创建 React Hook State
+    UIStateKeys.forEach(<T extends UIState>(stateKey: T) => {
+        /* eslint-disable-next-line react-hooks/rules-of-hooks */
+        const [state, setState] = useState<UIStateTypeMap[T]>(UIStateInitialValue[stateKey])
+        stateMap.set(stateKey, state)
+        setStateMap.set(stateKey, setState)
+    })
 
-  const getState = <T extends UIState>(stateKey: T) =>
-    stateMap.get(stateKey) as UIStateTypeMap[T]
-  const setState = <T extends UIState>(stateKey: T) =>
-    setStateMap.get(stateKey) as Dispatch<SetStateAction<UIStateTypeMap[T]>>
+    // 返回对 Map 的 getter setter 接口函数
+    const getState = <T extends UIState>(stateKey: T) => stateMap.get(stateKey) as UIStateTypeMap[T]
+    const setState = <T extends UIState>(stateKey: T) => setStateMap.get(stateKey) as Dispatch<SetStateAction<UIStateTypeMap[T]>>
+        
+    console.debug('context rendered', stateMap, setStateMap)
 
-  console.log('render context', stateMap, setStateMap)
-
-  return { getState, setState }
+    return { getState, setState }
 }
 
 // 创建 Constate Context
 const [UIStateProvider, ...hooks] = constate(
-  useUIStateContext,
-  // 为每个 stateKey 拆分 getter 和 setter hook 出来
-  ...UIStateKeys.map(
-    stateKey => (ctx: UIStateContext) => ctx.getState(stateKey)
-  ),
-  ...UIStateKeys.map(
-    stateKey => (ctx: UIStateContext) => ctx.setState(stateKey)
+    useUIStateContext,
+    // 为每个 stateKey 拆分 getter 和 setter hook 出来
+    ...UIStateKeys.map((stateKey) => (ctx: UIStateContext) => ctx.getState(stateKey)),
+    ...UIStateKeys.map((stateKey) => (ctx: UIStateContext) => ctx.setState(stateKey))
   )
 )
-
 // 将 hooks 分组存到两个 Index 索引中
 const UIGetState = UIStateKeys.reduce((cur, key, index) => {
-  cur[key] = hooks[index] as any
-  return cur
+    cur[key] = hooks[index] as any
+    return cur
 }, {} as { [key in UIState]: () => UIStateTypeMap[key] })
 const UISetState = UIStateKeys.reduce((cur, key, index) => {
-  cur[key] = hooks[index + hooks.length / 2] as any
-  return cur
+    cur[key] = hooks[index + hooks.length / 2] as any
+    return cur
 }, {} as { [key in UIState]: () => Dispatch<SetStateAction<UIStateTypeMap[key]>> })
 
 // 对外提供符合习惯的 hook
 export const useGetState = <T extends UIState>(key: T) => UIGetState[key]()
 export const useSetState = <T extends UIState>(key: T) => UISetState[key]()
 
+// 在组件中使用
 function Outer() {
   console.log('render outer')
 
